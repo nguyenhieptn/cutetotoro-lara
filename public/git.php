da7a74e9b2639b5fd7ab937f9cfd9b7085e787be
Pooe718 <dinhvanhieu927@gmail.com>
2023-02-02 16:09:38 +0700

    dvh

--- public/git.php
+++ public/git.php
@@ -1,5 +1,5 @@
 <?php
-$out = shell_exec( 'cd ..;git reset --hard;git pull origin main 2>&1' );
+$out = shell_exec( 'cd ..;git reset --hard;git pull origin master 2>&1' );
 
 echo '<pre>'.$out.'</pre>';
 
