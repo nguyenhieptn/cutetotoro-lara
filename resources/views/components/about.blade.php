2b436be37b8084b5f94c0af619a408234db56994
Caubehayho <longvanh2000@gmail.com>
2023-02-04 08:30:28 +0700

    ntl

---
+++ resources/views/components/about.blade.php
@@ -0,0 +1,28 @@
+<div class="about">
+    <div class="container">
+        <div class="row">
+            <div class="col align-item-center d-flex">
+                <div class="about-l justify-content-center align-item-center d-flex" style="flex-direction: column">
+                    <h1 style="font-size: 25px; text-align: center;">
+                        ABOUT US
+                    </h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                         eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="about-img">
+                    <img src="{{ asset('FrontEnd/Image/about.PNG') }}" alt="">
+                </div>
+            </div>
+        </div>
+        {{-- <div class="about-img">
+           <img src="{{ asset('FrontEnd/Image/ABOUT US 1.png') }}" alt="">
+        </div> --}}
+    </div>
+</div>
