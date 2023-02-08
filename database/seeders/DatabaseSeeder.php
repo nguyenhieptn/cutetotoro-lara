05ca110dfb508028a140fc06dd2e3a565f598646
nguyen minh duc <nguyenminhduc.sj@gmail.com>
2023-02-08 09:30:04 +0700

    wibulord

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(TruncateAllTables::class);
         $data = [
             [
                 'siteid' => '1',
