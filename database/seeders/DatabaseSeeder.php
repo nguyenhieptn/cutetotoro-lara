6716e05778e797adbf52a39238cec16f9d283cee
Caubehayho <longvanh2000@gmail.com>
2023-02-08 09:28:12 +0700

    ntl

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,104 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $data = [
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 0,
+                'code' => 'mug',
+                'label' => 'Mug',
+                'url' => 'mug',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'mug',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 1,
+                'code' => 'tshrit',
+                'label' => 'T-shrit',
+                'url' => 'tshrit',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'tshrit',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 1,
+                'code' => 'sticker',
+                'label' => 'Sticker',
+                'url' => 'sticker',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'sticker',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 1,
+                'code' => 'poster',
+                'label' => 'Poster',
+                'url' => 'poster',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'poster',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 1,
+                'code' => 'bag',
+                'label' => 'Bag',
+                'url' => 'bag',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'bag',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+            [
+                'siteid' => '1',
+                'parentid' => 1,
+                'level' => 1,
+                'code' => 'orther',
+                'label' => 'Orther',
+                'url' => 'orther',
+                'config' => '',
+                'nleft' => 1,
+                'nright' => 1,
+                'target' => 'orther',
+                'status' => 1,
+                'mtime' => now(),
+                'ctime' => now(),
+                'editor' => 'Admin@aimeos.laravel'
+            ],
+        ];
+        DB::table('mshop_catalog')->insert($data);
     }
 }
