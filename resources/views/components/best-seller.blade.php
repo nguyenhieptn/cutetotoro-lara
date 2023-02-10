c2d1af3eaa9c74ae75d197512b4980bf8984090b
nguyen minh duc <nguyenminhduc.sj@gmail.com>
2023-02-10 15:08:13 +0700

    testcode

--- resources/views/components/best-seller.blade.php
+++ resources/views/components/best-seller.blade.php
@@ -1,7 +1,11 @@
 <?php
 use Illuminate\Support\Facades\DB;
-$info_product =  DB::table('mshop_text')->join('mshop_media','mshop_text.mtime','=','mshop_media.mtime')->join('mshop_price','mshop_text.mtime','=','mshop_price.mtime')->join('mshop_product_list','mshop_text.mtime','=','mshop_product_list.mtime')->join('mshop_product','mshop_product_list.parentid','=','mshop_product.id')->get();
-dd($info_product);
+$info_product = DB::table('mshop_text')
+    ->join('mshop_media', 'mshop_text.mtime', '=', 'mshop_media.mtime')
+    ->join('mshop_price', 'mshop_text.mtime', '=', 'mshop_price.mtime')
+    ->join('mshop_product_list', 'mshop_text.mtime', '=', 'mshop_product_list.mtime')
+    ->join('mshop_product', 'mshop_product_list.parentid', '=', 'mshop_product.id')
+    ->get();
 ?>
 <div class="best_seller">
     <div class="container container-globe">
@@ -17,14 +21,31 @@ dd($info_product);
         <div class="row">
             <a href="/all-product" class="a-view-all text-end">View all</a>
             <div class="main-carousel row"
-                 data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true, "adaptiveHeight": true}'>
+                data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true, "adaptiveHeight": true}'>
                 {{-- "autoPlay": 3000 --}}
-                
+                <div class="carousel-cell" style="position: relative">
+                    @foreach ($info_product as $key => $info )
+                        <a href="/product-detail"><img src="/aimeos/{{ $data->link }}"></a>
+                        <div class="best_seller_bottom">
+                            <span>
+                                {{ $data->content }}
+                            </span>
+                            <div class="best_seller-price">
+                                <p style="display: flex; align-item: center"><strike
+                                        style="line-height: 24px">{{ $data->value }}$</strike></p>
+                                <?php
+                                $sale = $data->value - $data->rebate;
+                                ?>
+                                <p>{{ $sale }}$</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
 </section>
-<link rel="stylesheet" href="{{ asset('FrontEnd/Css/main-details.css?v='.time()) }}">
+<link rel="stylesheet" href="{{ asset('FrontEnd/Css/main-details.css?v=' . time()) }}">
 <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
