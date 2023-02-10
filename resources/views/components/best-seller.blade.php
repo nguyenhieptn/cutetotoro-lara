24b170287c884eb73846a0e96e52df17e09c2a70
nguyen minh duc <nguyenminhduc.sj@gmail.com>
2023-02-10 15:30:09 +0700

    wibulord

--- resources/views/components/best-seller.blade.php
+++ resources/views/components/best-seller.blade.php
@@ -21,31 +21,31 @@ $info_product = DB::table('mshop_text')
         <div class="row">
             <a href="/all-product" class="a-view-all text-end">View all</a>
             <div class="main-carousel row"
-                data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true, "adaptiveHeight": true}'>
+                 data-flickity='{ "cellAlign": "left", "contain": true, "wrapAround": true, "adaptiveHeight": true}'>
                 {{-- "autoPlay": 3000 --}}
-                <div class="carousel-cell" style="position: relative">
-                    @foreach ($info_product as $key => $info )
-                        <a href="/product-detail"><img src="/aimeos/{{ $info->link }}"></a>
-                        <div class="best_seller_bottom">
-                            <span>
-                                {!! $info->content !!}
-                            </span>
-                            <div class="best_seller-price">
-                                <p style="display: flex; align-item: center"><strike
-                                        style="line-height: 24px">{{ $info->value }}$</strike></p>
-                                <?php
-                                $sale = $info->value - $info->rebate;
-                                ?>
-                                <p>{{ $sale }}$</p>
-                            </div>
+                @foreach ($info_product as $key => $data)
+                        <div class="carousel-cell" style="position: relative">
+                            <a href="/product-detail"><img src="/aimeos/{{$data->link }}"></a>
+                                {{-- class="girl img-responsive" alt="" /> --}}
+                                <div class="best_seller_bottom">
+                                    <span>
+                                        {!! $data->content !!}
+                                    </span>
+                                    <div class="best_seller-price">
+                                        <p style="display: flex; align-item: center"><strike style="line-height: 34px">{{ $data->value }}$</strike></p>
+                                        <?php
+                                            $sale = $data->value - $data->rebate
+                                        ?>
+                                        <p>{{ $sale}}$</p>
+                                    </div>
+                                </div>
                         </div>
-                    @endforeach
-                </div>
+               @endforeach
             </div>
         </div>
     </div>
 </section>
-<link rel="stylesheet" href="{{ asset('FrontEnd/Css/main-details.css?v=' . time()) }}">
+<link rel="stylesheet" href="{{ asset('FrontEnd/Css/main-details.css?v='.time()) }}">
 <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
