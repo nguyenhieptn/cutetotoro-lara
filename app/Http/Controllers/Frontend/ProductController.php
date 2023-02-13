be87241c690a9c02228b1741257b69974441f1e2
nguyen minh duc <nguyenminhduc.sj@gmail.com>
2023-02-13 08:59:48 +0700

    sen

--- app/Http/Controllers/Frontend/ProductController.php
+++ app/Http/Controllers/Frontend/ProductController.php
@@ -13,11 +13,22 @@ class ProductController extends Controller
 {
     public function detail($id): \Illuminate\Http\Response
     {
+        $product = DB::table('mshop_product_list')
+            ->join('mshop_media', 'mshop_product_list.refid', '=', 'mshop_media.id')
+            ->join('mshop_text', 'mshop_product_list.refid', '=', 'mshop_text.id')
+            ->join('mshop_price', 'mshop_product_list.refid', '=', 'mshop_price.id')
+            ->join('mshop_product', 'mshop_product_list.parentid', '=', 'mshop_product.id')
+            ->where('parentid', $idProduct)
+            ->first();
+        $salePrice = $product->value - $product->rebate;
+        if ($salePrice < 0) {
+            $salePrice = 0;
+        }
         $productManager = MShop::create(app('aimeos.context')->get(), 'product');
         $product = $productManager->get($id, ['text', 'media', 'price', 'catalog']);
         $product = convertAimeosProductToProduct($product);
         view()->share('product', $product);
-        return Response::view(Shop::template('product.index'), [])
+        return Response::view(Shop::template('product.index'), [])->with(compact('product', 'salePrice'))
             ->header('Cache-Control', 'no-store, , max-age=0');
     }
 
