6225fb777afc5f776dcdc54580c2ff80cad8c80b
Senkiysma <nguyenminhduc.sj@gmail.com>
2023-02-17 18:18:52 +0700

    sen

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,92 @@
+
+ 
+<link rel="stylesheet" href="{{ asset('FrontEnd/Css/best-seller.css') }}">
+<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+<div class='container d-flex ft'>
+    <div class="row">
+        <div class="col col-12 d-flex col-ft">
+            <div class='a' >
+                <p>ABOUT US</p>
+                <p>We founded Cute Totoro with one simple goal:</p>
+                <p>To make you so special and so cute with Totoro.</p>
+                <p>Join us if you also love Totoro</p>
+                <p>#cutetotoro</p>
+            </div>
+            <div>
+                <img class="line" src="{{ asset('/FrontEnd/Image/Line2.png') }}" alt="" with: 100%
+                    height: 100%>
+            </div>
+            <div class='a'>
+                <p>CONTACT US</p>
+                <p>Email: cutetoro.com@gmail.com</p>
+                <p>Phone: +1 (740) 291-3866</p>
+                <p>Mon - Fri: 08:00 AM to 06:00 PM GMT+9</p>
+            </div>
+            <div>
+                <img class="line" src="{{ asset('/FrontEnd/Image/Line2.png') }}" alt="" with: 100%
+                    height: 100%>
+            </div>
+            <div class='a'>
+                <p>INFORMATION</p>
+                <ul>
+                    <li class='b'>Wishlist</li>
+                    <li class='b'>About us</li>
+                    <li class='b'>Contact us</li>
+                    <li class='b'>Shipping Policy</li>
+                    <li class='b'>Terms of Service</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid footer-botom"
+    style="padding-top: 30px; padding-bottom: 30px; background-color: #d9d9d9; margin-top: 30px; margin-bottom: -16px;">
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-xs-12 col-xl-6">
+                <div class="ft-bt-left">
+                    <ul style="list-style: none; padding-left: 0; margin-bottom: 0px">
+                        <li><a href="#">wishlist</a></li>
+                        <li><a href="#">about us</a></li>
+                        <li><a href="#">contact us</a></li>
+                        <li><a href="#"> shipping policy</a></li>
+                        <li><a href="#">terms of service</a></li>
+                    </ul>
+                    <p class="d-flex justify-content-center" style="margin-top: 15px; margin-bottom: 0px">copyright 2023
+                        © cutetotoro</p>
+                </div>
+            </div>
+
+            <div class="col col-12 col-xs-12 col-xl-6">
+                <div class="ft-bt-right d-flex justify-content-center">
+                    <p style="margin-bottom: 0px">Contact with us:</p>
+                    <ul style="list-style: none; padding-left: 0px; margin-bottom: 0px">
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-telegram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
