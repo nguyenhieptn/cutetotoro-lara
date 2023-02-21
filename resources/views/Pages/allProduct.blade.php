89d5a277be1cebdf59dcd25c5e174e6670af7f25
Senkiysma <nguyenminhduc.sj@gmail.com>
2023-02-21 11:28:35 +0700

    senkiysma

--- resources/views/Pages/allProduct.blade.php
+++ resources/views/Pages/allProduct.blade.php
@@ -77,7 +77,7 @@
         </div>
         <div class="element" style="margin-top: 81px;">
             @foreach ($bestSeller as $key => $dt)
-                <div class="element-item {{ $dt['catalog'][0]['label'] }} {{ $dt['product.config']['Color'] }}">
+                <div class="element-item {{ $dt['catalog'][0]['label'] }}">
                     <div class="mug-product">
                         <a href="{{ route('product.detail',$dt['product.id'] ) }}" style="display: block">
                             <div class="mug-img">
