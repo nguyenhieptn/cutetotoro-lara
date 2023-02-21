6e458b89bb1f54e2e9c64a17b85bae0bc3b21fd9
Senkiysma <nguyenminhduc.sj@gmail.com>
2023-02-21 16:20:42 +0700

    senkiysma

---
+++ resources/views/Pages/header_s.blade.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/5bf87cd97a.js" crossorigin="anonymous"></script>
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('FrontEnd/Css/StyleHomePage.css') }}"> --}}
+    <!-- font google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Manjari:wght@100;700&display=swap" rel="stylesheet">
+    <title>Cute Totoro</title>
+    <link rel="stylesheet" href="{{ asset('FrontEnd/Css/best-seller.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
--- resources/views/Pages/poster/poster.blade.php
+++ resources/views/Pages/poster/poster.blade.php
@@ -1,101 +1,83 @@
-@extends('cutetotoro::base')
-@section('content')
-    <link rel="stylesheet" href="{{ asset('FrontEnd/Css/best-seller.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <div>
-        <h1 style="color: white; margin-top: 34px;">
-            <center>
-                T-shirt
-            </center>
-        </h1>
+@include('Pages.header_s')
+<body>
+@include('cutetotoro::layouts.head')
+<div class="container">
+    <div class="mug-link">
+        <p class="d-flex">
+            <a href="/">Home </a>
+            <span style="margin: 0 10px;">/</span>Mug
+        </p>
     </div>
-    <div class="container">
-        <div class="mug-link">
-            <p class="d-flex">
-                <a href="/">Home </a>
-                <span style="margin: 0 10px;">/</span><span>T-</span><span>shirt</span>
-            </p>
+    <div class="row">
+        <div class="mug-banner">
+            <img src="{{ asset('FrontEnd/Image/mug/banner.png') }}" alt="banner" style="width: 100%;">
         </div>
-        <div class="row">
-            <div class="mug-banner">
-                <img src="{{ asset('FrontEnd/Image/mug/banner.png') }}" alt="banner" style="width: 100%;">
+    </div>
+    <div class="row">
+        <div class="mug-button d-flex justify-content-end dropdown">
+            <button type="button" class="mug-btn " data-bs-toggle="dropdown" aria-expanded="false">price
+                <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            <button type="button" class="mug-btn " data-bs-toggle="dropdown"
+                    aria-expanded="false">colors
+                <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            <button type="button" class="mug-btn " data-bs-toggle="dropdown" aria-expanded="false">type
+                <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            <div class="d-flex align-items-center">
+                <img src="{{ asset('FrontEnd/Image/toright.png') }}" alt="toright">
             </div>
         </div>
-        <div class="row">
-            <div class="mug-button d-flex justify-content-end dropdown">
-                <button type="button" class="mug-btn " data-bs-toggle="dropdown" aria-expanded="false">price
-                    <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                <button type="button" class="mug-btn " data-bs-toggle="dropdown"
-                        aria-expanded="false">colors
-                    <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                <button type="button" class="mug-btn " data-bs-toggle="dropdown" aria-expanded="false">type
-                    <img src="{{ asset('FrontEnd/Image/down.png') }}" alt="down">
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-                <div class="d-flex align-items-center">
-                    <img src="{{ asset('FrontEnd/Image/toright.png') }}" alt="toright">
-                </div>
-            </div>
-        </div>
-        <div class="element" style="margin-top: 81px;">
-            @if($filteredArray == NULL)
-                <h1 style="color: red; text-align: center">Not Item</h1>
-            @endif
-            @foreach ($filteredArray as $key => $dt)
-                <div class="element-item">
-                    <div class="mug-product">
-                        <a href="{{ route('product.detail',$dt['product.id'] ) }}" style="display: block">
-                            <div class="mug-img">
-                                <img src="/aimeos/{{ $dt['media'][0] }}" class="girl img-responsive" alt=""/>
-                                <div class="mug-sticker" >
-                                    <img src="{{ asset('FrontEnd/Image/heart.png') }}" alt="mug-layer">
-                                    <img src="{{ asset('FrontEnd/Image/cart0.png') }}" alt="mug-layer">
-                                </div>
+    </div>
+    @if($filteredArray == null)
+        <h1 style="color: red">
+            <center>Not item in page</center>
+        </h1>
+    @endif
+    <div class="element" style="margin-top: 81px;">
+        @foreach ($filteredArray as $key => $dt)
+            <div class="element-item">
+                <div class="mug-product">
+                    <a href="{{ route('product.detail',$dt['product.id'] ) }}" style="display: block">
+                        <div class="mug-img">
+                            <img src="/aimeos/{{ $dt['media'][0] }}" class="girl img-responsive" alt=""/>
+                            <div class="mug-sticker" >
+                                <img src="{{ asset('FrontEnd/Image/heart.png') }}" alt="mug-layer">
+                                <img src="{{ asset('FrontEnd/Image/cart0.png') }}" alt="mug-layer">
                             </div>
-                            <div class="best_seller_bottom">
+                        </div>
+                        <div class="best_seller_bottom">
                                 <span>
                                     {!! $dt['text'][0] !!}
                                 </span>
-                                <div class="best_seller-price">
-                                    <p style="display: flex; align-item: center; margin-right: 15px">
-                                        <strike>{{ $dt['price'][0]['actual'] }}$</strike>
-                                    </p>
-                                    <p>{{ $dt['price'][0]['actual'] - $dt['price'][0]['rebate'] }}$</p>
-                                </div>
+                            <div class="best_seller-price">
+                                <p style="display: flex; align-item: center; margin-right: 15px">
+                                    <strike>{{ $dt['price'][0]['actual'] }}$</strike>
+                                </p>
+                                <p>{{ $dt['price'][0]['actual'] - $dt['price'][0]['rebate'] }}$</p>
                             </div>
-                        </a>
-                    </div>
+                        </div>
+                    </a>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
-    <footer>
-        <!-- footer page -->
-
-        <!-- script -->
-
-        <script src="{{ asset('js/isotope.pkgd.min.js') }}"></script>
-        <script>
-            var $element = $('.element').isotope({
-                itemSelector: '.element-item',
-                layoutMode: 'fitRows'
-            });
-        </script>
-    </footer>
-@stop
+</div>
+</body>
+@include('Pages.footer_s')
